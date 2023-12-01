7f57f4c0ca82b57941630f5772402e26629e9f67
PSamardzhiev <petko9001@gmail.com>
2023-12-01 19:45:35 +0200

    The script can now check if the import directory and the CVS file are present

--- usersadimport.ps1
+++ usersadimport.ps1
@@ -1,7 +1,31 @@
 # Specify the path to the CSV file 
 #you can change your path based on your CSV location
 
+$tmpdir = "C:\temp\"
 $csvPath = "C:\temp\users.csv"
+
+
+if (!(Test-Path $tmpdir -ErrorAction SilentlyContinue)) {
+    Write-Host "The script is unable to find the required path -> '$tmpdir'"
+    Start-Sleep 10
+    Write-Host "The script will try to create the following path -> '$tmpdir'"
+    Start-Sleep 10
+    try {
+    New-Item -ItemType Directory -Path $tmpdir
+    }
+    catch {
+        Write-Host "the directory '$tmpdir' cannot be created $_"
+        Start-Sleep 10
+    }
+}
+if (!(Test-Path $csvPath -ErrorAction Stop)) {
+    Write-Host "The script cannot find the users.csv file located at '$tmpdir'"
+    Start-Sleep 3
+    Write-Host "Please copy the CSV file which you want to import in Active Directory and rename it to users.csv"
+    Start-Sleep 5
+    break
+
+}
 
 # Import user data from CSV
 $userData = Import-Csv $csvPath
@@ -45,10 +69,11 @@ foreach ($user in $userData) {
     $email = $user."Email Address"
     $description = $user."Description"
     $enabled = $user."Enabled"
-    $password = $user."Password"
+    #$password = $user."Password" password defined below in a variable for easier use
+    $PSDString = 'P@ssw0rd2023'
 
     $enabled = $enabled -eq "TRUE"
-    $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+    $securePassword = ConvertTo-SecureString $PSDString -AsPlainText -Force
 
     try {
         # Check if the user already exists
@@ -62,7 +87,7 @@ foreach ($user in $userData) {
                  -ChangePasswordAtLogon $true
 
             Write-Host "User $sam created successfully." 
-            write-host "The user will be required to change his password at logon, the initial login password is $Password"
+            write-host "The user will be required to change his password at logon, the initial login password is $PSDString"
             Start-Sleep 1
         } else {
             Write-Host "User $sam already exists. The Script will skip this user!."
